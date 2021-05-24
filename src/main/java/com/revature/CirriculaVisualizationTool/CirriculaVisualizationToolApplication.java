ae5969b3079f5d13b04053e848014e33345140ba
michaelbmcgroary <81986562+michaelbmcgroary@users.noreply.github.com>
2021-05-24 17:02:29 -0400

    Add initial setup for spring boot

---
+++ src/main/java/com/revature/CirriculaVisualizationTool/CirriculaVisualizationToolApplication.java
@@ -0,0 +1,13 @@
+package com.revature.CirriculaVisualizationTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CirriculaVisualizationToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CirriculaVisualizationToolApplication.class, args);
+	}
+
+}
