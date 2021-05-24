94e199350f3c4dc63b1e116abc2a7b4f1906a664
michaelbmcgroary <81986562+michaelbmcgroary@users.noreply.github.com>
2021-05-24 17:02:29 -0400

    Add initial setup for spring boot

---
+++ src/main/java/com/revature/app/CurriculaVisualizationToolApplication.java
@@ -0,0 +1,13 @@
+package com.revature.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculaVisualizationToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculaVisualizationToolApplication.class, args);
+	}
+
+}
