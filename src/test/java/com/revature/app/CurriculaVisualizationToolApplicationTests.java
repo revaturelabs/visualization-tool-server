700bf7f4f534c2d97410635fb8a755ff101eba40
Joshua Hulbert <joshua.hulbert@revature.net>
2021-06-01 14:22:35 -0400

    fixed duplicated annotation

--- src/test/java/com/revature/app/CurriculaVisualizationToolApplicationTests.java
+++ src/test/java/com/revature/app/CurriculaVisualizationToolApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
-@ActiveProfiles("test")
 class CurriculaVisualizationToolApplicationTests {
 
 	@Test
